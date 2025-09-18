e16979ff3ae0d0ea37976aaa694da0ca34813561
MuxamedO <momadiin24@gmail.com>
2025-09-18 19:09:04 +0000

    start-up

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            how do you know react
           </a>
         </p>
       </header>
